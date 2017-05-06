9f1aaf622b4495e0aa00ecc0ccb9bb625fbc0cec
Ben Whitley <ben.whitley@hotmail.com>
2017-05-06 10:31:32 +0100

    Implement creating tickets for a project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
-  root "projects#index"
-  
 
-  resources :projects
+  root "projects#index"
+
+
+  resources :projects do
+    resources :tickets
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
